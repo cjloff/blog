108a1bf07f53331538382644afa9bb30dabe3a1f
cjloff <chesterjakeloffman@gmail.com>
2021-03-06 18:22:22 +0000

    update intro

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -5,7 +5,8 @@ export default () => (
     <section class="introduction">
         <h2 class="introduction__title">About me</h2>
         <p class="introduction__summary"> 
-I am a frontend developer with 7 years industry experience. I specialize in CSS, HTML and Javascript and have professional experience working in a C# and .NET framework environment. I am always keen to help problem solve and implement new ways of working in the fast-paced environment of frontend development. 
+        Creative and self-motivated frontend developer with 8 years of experience building websites and applications in fast-paced collaborative environments. Seasoned in CSS, HTML, Javascript and Umbraco integrations. I also enjoy keeping up to date with the latest technologies and techniques in the ever-moving landscape of frontend development.  
+
  </p>
     </section>
 )
