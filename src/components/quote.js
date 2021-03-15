f64df6d2261e0d8537759f7f232875e4e5e42308
cjloff <chesterjakeloffman@gmail.com>
2021-03-15 20:25:10 +0000

    FE

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,12 +1,45 @@
 import React from "react"
 import "./quote.scss"
+import { StaticQuery, graphql, Link } from "gatsby"
 
 export default () => (
     <section class="quote">
         <div class="quote__container">
-            <h2 class="quote__title">A few words from other people</h2>
-            <p class="quote__text">Chester has a superior knowledge of CSS and HTML but bar far his greatest asset is the ability to think outside the box and come up with solutions to tricky problems when they present themselves [...] I have worked with many developers over the years and the one thing that sets a good developer apart from a great developer is the ability to research problems quickly and find solid and relevant solutions using the latest technology.</p>
-            <p class="quote__author">David Birrell, Alyka</p>
+        <StaticQuery
+            query={graphql`
+                query BlogQuery {
+                    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+                        totalCount
+                        edges {
+                          node {
+                            id
+                            frontmatter {
+                              title
+                              date(formatString: "DD MMMM, YYYY")
+                            }
+                            fields {
+                              slug
+                            }
+                            excerpt(pruneLength: 300)
+                          }
+                        }
+                      }
+                }
+            `}
+            render={data => {
+                const latestBlogPost = data.allMarkdownRemark.edges[0].node
+                return(<div>
+                    <h2 class="quote__author">Latest Blog Post</h2>
+                    <div class="quote__main">
+                        <Link to={latestBlogPost.fields.slug} key={latestBlogPost.id}>
+                            <h3 class="quote__sub">{latestBlogPost.frontmatter.title}{" "}</h3>
+                            <p class="quote__text">{latestBlogPost.excerpt}</p>
+                        </Link>
+                    </div>
+                </div>)
+            }}
+            />
+    
         </div>
     </section>
 )
